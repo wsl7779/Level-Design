ecdddf8277c733ee4f2a940ed3c3daf6a4c2ed8f
jx1279 <jx1279@nyu.edu>
2024-02-21 16:47:22 -0500

    Revert "Untested- added code for death sound"
    This reverts commit f588724ba4f57ba2d5c8d4838215b17fd90cae8f.

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour
 {
-    [SerializeField] private AudioClip deathSound;
-    private AudioSource audioSource;
-
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -19,20 +16,12 @@ public class EnemyHandler : MonoBehaviour
         
     }
 
-    private void deathSoundPlay() {
-        audioSource.clip = deathSound;
-        audioSource.Play();
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            deathSoundPlay();
             MovementController mc = collision.transform.GetComponent<MovementController>();
             mc.Reset();
         }
     }
-
 }
