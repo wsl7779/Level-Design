e64274cfe14bd78503245327b53a380ebbc9b923
Will Lee <144484482+wsl7779@users.noreply.github.com>
2024-02-21 17:32:24 -0500

    lever

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -6,6 +6,21 @@ public class SwitchScript : MonoBehaviour
 {
 
     public bool isOn = false;
+    public SpriteRenderer spriterend;
+    public Sprite newSprite1;
+    public Sprite newSprite2;
+
+    void Update ()
+    {
+        if (isOn)
+        {
+            spriterend.sprite = newSprite1;
+        }
+        else
+        {
+            spriterend.sprite = newSprite2;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
